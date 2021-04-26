9db7669cdf3aaa48a71107e9dec340dc5d8ba0ae
yamanaka-sra <yamanaka@sra-tohoku.co.jp>
2021-04-26 17:47:24 +0900

    問合せ一覧（Mobile） ・問合せ一覧機能追加
    問合せフォーム（Mobile）
    ・問合せフォーム機能追加

--- www/js/contact.js
+++ www/js/contact.js
@@ -1,64 +1,185 @@
+// 遷移元画面ID
+var transitionId = 0;
 // 調査ID
-var contactId = null;
+var surveyId = "";
+// 調査明細ID
+var surveyDetailId = "";
 
 document.addEventListener("deviceready", async function () {
-    contactId = location.search.substring(1);
+    var param = location.search.substring(1).split("&");
+    let contactId = param[0];
+
+    transitionId = parseInt(param[1]);
+
+    if (param.length > 2) {
+        surveyId = param[2];
+    }
+    if (param.length > 3) {
+        surveyDetailId = param[3];
+    }
+
+    // 遷移元画面名
+    var functionName = "";
+    switch (transitionId) {
+        case 1:
+            functionName = "調査業務一覧";
+            break;
+        case 2:
+            functionName = "所在地一覧";
+            break;
+        case 3:
+            functionName = "毎木調査登録";
+            break;
+        case 4:
+            functionName = "伐採木データ履歴一覧";
+            break;
+        case 5:
+            functionName = "伐採木一覧";
+            break;
+        case 6:
+            functionName = "小径木登録";
+            break;
+    }
 
     // 初期表示
-    initView();
+    initView(contactId, functionName);
 });
 
 /**
  * 初期表示
+ * @param {number} contactId 問合せID
+ * @param {string} functionName 遷移元画面名
  */
-async function initView() {
+async function initView(contactId, functionName) {
     // 問合せデータ取得
-    // TODO ajax
-    var test = true;
-    if (test) {
-        // TODO 問合せデータ設定
-        setTargetContact();
-    } else {
+    if (contactId > 0) {
+        // 問合せデータ設定
+        setTargetContact(contactId);
+    }
+    else {
         // 初期表示
-        setInitContact();
+        setInitContact(functionName);
     }
 }
 /**
  * 問合せフォームの初期表示
+ * @param {string} functionName 遷移元画面名
  */
-function setInitContact() {
+function setInitContact(functionName) {
     // 件名
     $('#contact-name').val("");
+    $('#contact-name').prop("disabled", false);
     // 氏名
     $('#user-name').val("");
-    // TODO 問合せ区分
-    // TODO 問合せ画面
+    $('#user-name').prop("disabled", false);
+    // 問合せ区分
+    $('#contact-class').val("question");
+    // 問合せ画面
+    $('#contact-function').val(functionName);
     // 問合せ内容
     $('#contact-message').val("");
+    $('#contact-message').prop("disabled", false);
     // 問合せ回答非表示
     $('#contact-answer-field').hide();
+    // 問合せ内容送信ボタン使用可
+    $('#register').css("pointer-events", "auto");
 }
 
 /**
  * 取得した問合せデータを設定
+ * @param {string} contactId 問合せID
  */
-function setTargetContact() {
-    // 件名
-    $('#contact-name').val("調査業務データの変更方法について");
-    $('#contact-name').prop("disabled", true);
-    // 氏名
-    $('#user-name').val("匿名");
-    $('#user-name').prop("disabled", true);
-    // TODO 問合せ区分
-    // TODO 問合せ画面
-    // 問合せ内容
-    $('#contact-message').val("調査業務の変更ができないですが、どのように操作する必要があるのか教えてください。");
-    $('#contact-message').prop("disabled", true);
-    // 問合せ回答
-    $('#contact-answer').val("回答");
-    $('#contact-answer').prop("disabled", true);
+function setTargetContact(contactId) {
+    var item = localStorage.getItem(KEY);
+    var obj = JSON.parse(item);
+
+    var JSONdata = {
+        id: parseInt(contactId)
+    };
+
+    $.ajax({
+        type: 'post',
+        url: path + 'contact/edit',
+        data: JSON.stringify(JSONdata),
+        contentType: 'application/json',
+        dataType: 'JSON',
+        scriptCharset: 'utf-8',
+        async: false,
+        headers: { 'Authorization': obj.token }
+    })
+        .done(async (data) => {
+            var jsonData = JSON.stringify(data);
+            var responseData = JSON.parse(jsonData);
+
+            // 問合せ情報
+            var contactInfo = responseData.contactInfo;
+
+            // 件名
+            $('#contact-name').val(contactInfo.contact_name);
+            $('#contact-name').prop("disabled", true);
+            // 氏名
+            $('#user-name').val(contactInfo.user_name);
+            $('#user-name').prop("disabled", true);
+            // 問合せ区分
+            $('#contact-class').val(contactInfo.contact_class);
+            // 問合せ画面
+            $('#contact-function').val(contactInfo.contact_function);
+            // 問合せ内容
+            $('#contact-message').val(contactInfo.contact_message);
+            $('#contact-message').prop("disabled", true);
+            // 問合せ回答
+            $('#contact-answer').val(contactInfo.contact_answer);
+            $('#contact-answer').prop("disabled", true);
+            // 問合せ内容送信ボタン使用不可
+            $('#register').css("pointer-events", "none");
+        })
+        .fail(async (jqXHR, textStatus, errorThrown) => {
+            var jsonData = JSON.stringify(jqXHR);
+            var responseData = JSON.parse(jsonData);
+            errorProcessByGetContact(responseData);
+        })
 }
 
+/**
+ * 問合せ登録処理
+ */
+function save() {
+    // 入力チェック
+    if (validate()) {
+        var item = localStorage.getItem(KEY);
+        var obj = JSON.parse(item);
+
+        var JSONdata = {
+            contact_name: $('#contact-name').val(),
+            user_name: $('#user-name').val(),
+            contact_class: $('#contact-class').val(),
+            contact_function: $('#contact-function').val(),
+            contact_message: $('#contact-message').val(),
+            status: "unsupported",
+            userId: fetchUserId()
+        };
+
+        $.ajax({
+            type: 'post',
+            url: path + 'contact/save',
+            data: JSON.stringify(JSONdata),
+            contentType: 'application/json',
+            dataType: 'JSON',
+            scriptCharset: 'utf-8',
+            async: false,
+            headers: { 'Authorization': obj.token }
+        })
+            .done(async (data) => {
+                // 問合せ一覧表示
+                location.href = "../html/contact_list.html?" + transitionId + "&" + surveyId + "&" + surveyDetailId + "&" + 1;
+            })
+            .fail(async (jqXHR, textStatus, errorThrown) => {
+                var jsonData = JSON.stringify(jqXHR);
+                var responseData = JSON.parse(jsonData);
+                errorProcessByRegisterContact(responseData);
+            })
+    }
+}
 /**
  * 入力チェック
  */
@@ -87,7 +208,7 @@ function validate() {
  * @param {*} 必須チェック有無
  * @param {*} 数値チェック有無
  */
- function updateInputTextArea(inputdata, column, required) {
+function updateInputTextArea(inputdata, column, required) {
     if (required && validateRequired(inputdata, column)) {
         return;
     }
@@ -107,4 +228,58 @@ function validateRequired(inputdata, column) {
     $('#' + column + '_required').hide();
     inputdata.style.background = 'white';
     return false;
+}
+
+/**
+ * 問合せ取得結果がエラーだった場合の処理
+ * @param {object} responseData
+ */
+async function errorProcessByGetContact(responseData) {
+    var message = '';
+    var error   = '';
+    if (responseData.status == 401) {
+        message = '申し訳ございません。セッションタイムアウトエラーが発生しました。再度ログインしてから処理を実行してください。';
+        error   = 'セッションタイムアウトエラー';
+    }
+    else if (responseData.status == 0) {
+        // API接続なし
+        // NW接続なし
+        message = 'ネットワークエラーが発生しました。ネットワークに接続されていることを確認し、もう一度件名リンクをクリックしてください。確認後も同一の現象が発生する場合は、管理者へお問合せください。';
+        error   = 'サーバ接続エラー';
+    }
+    else {
+        message = '問合せ取得処理が失敗しました。もう一度処理を実行してください。実行後も同一の現象が発生する場合は、管理者へお問合せください。';
+        error   = responseData.responseJSON.message;
+    }
+
+    $('#error').text(error);
+    $('#errorMessage').text(message);
+    $('#contactError').modal('open');
+}
+
+/**
+ * 問合せ登録結果がエラーだった場合の処理
+ * @param {object} responseData
+ */
+async function errorProcessByRegisterContact(responseData) {
+    var message = '';
+    var error   = '';
+    if (responseData.status == 401) {
+        message = '申し訳ございません。セッションタイムアウトエラーが発生しました。再度ログインしてから処理を実行してください。';
+        error   = 'セッションタイムアウトエラー';
+    }
+    else if (responseData.status == 0) {
+        // API接続なし
+        // NW接続なし
+        message = 'ネットワークエラーが発生しました。ネットワークに接続されていることを確認し、もう一度「問合せ内容送信」ボタンをクリックしてください。確認後も同一の現象が発生する場合は、管理者へお問合せください。';
+        error   = 'サーバ接続エラー';
+    }
+    else {
+        message = '問合せ登録処理が失敗しました。もう一度処理を実行してください。実行後も同一の現象が発生する場合は、管理者へお問合せください。';
+        error   = responseData.responseJSON.message;
+    }
+
+    $('#error').text(error);
+    $('#errorMessage').text(message);
+    $('#contactError').modal('open');
 }
--- www/js/contact_list.js
+++ www/js/contact_list.js
@@ -1,23 +1,34 @@
+// 遷移元画面ID
+var transitionId   = 0;
+// 調査ID
+var surveyId       = "";
+// 調査明細ID
+var surveyDetailId = "";
+// 登録フラグ
+var isRegister     = 0;
+
 document.addEventListener("deviceready", function () {
   var param = location.search.substring(1).split("&");
 
   // 遷移元ID取得
-  var transitionId = parseInt(param[0]);
+  transitionId = parseInt(param[0]);
   // 調査ID取得
-  var surveyId = "";
   if (param.length > 1) {
     surveyId = param[1];
   }
   // 調査明細ID取得
-  var surveyDetailId = "";
   if (param.length > 2) {
     surveyDetailId = param[2];
+  }
+  // 登録フラグ取得
+  if (param.length > 3) {
+    isRegister = parseInt(param[3]);
   }
 
   // 戻るタグ作成
   createBackLink(transitionId, surveyId, surveyDetailId);
 
-  // TODO 一覧作成
+  // 一覧作成
   showContactList();
 });
 
@@ -26,49 +37,137 @@ document.addEventListener("deviceready", function () {
  */
 function showContactList() {
   $('#contact-item').empty();
-  var contactItem = $('#contact-item');
-  // TODO 仮の一覧データ作成
-  var dataCount = 3;
-  var texts = '';
-  if (dataCount === 0) {
-    texts += '<div class="col s12 m7">';
-    texts += '<div class="card horizontal">';
-    texts += '<div class="card-stacked">';
-    texts += '<div class="card-content">';
-    texts += '<p>データが存在しません。</p>';
-    texts += '</div>';
-    texts += '</div>';
-    texts += '</div>';
-    texts += '</div>';
-  } else {
-    for (var i = 0; i < dataCount; i++) {
-      texts += '<div class="col s12 m7">';
-      texts += '<div class="card horizontal">';
-      texts += '<div class="card-stacked">';
-      texts += '<div class="card-content">';
-      texts += '問合せ区分：' + '質問';
-      texts += '<br>';
-      texts += '件名：<a href="../html/contact.html?' + 1 + '">' + convertSpace('調査業務データの変更方法について') + '</a>';
-      texts += '<br>';
-      texts += '問合せ状態：' + '未対応';
-      texts += '<br>';
-      texts += '問合せ日時：' + '2021/4/23';
-      texts += '</div>';
-      texts += '</div>';
-      texts += '</div>';
-      texts += '</div>';
-    }
+
+  var item = localStorage.getItem(KEY);
+  var obj  = JSON.parse(item);
+
+  var JSONdata = {
+    userId: fetchUserId()
+  };
+
+  $.ajax({
+    type          : 'post',
+    url           : path + 'contact/list',
+    data          : JSON.stringify(JSONdata),
+    contentType   : 'application/json',
+    dataType      : 'JSON',
+    scriptCharset : 'utf-8',
+    async         : false,
+    headers       : { 'Authorization': obj.token }
+  })
+    .done(async (data) => {
+      var jsonData     = JSON.stringify(data);
+      var responseData = JSON.parse(jsonData);
+
+      // 一覧データ作成
+      var contactItem = $('#contact-item');
+      var texts       = '';
+      var contactList = responseData.contactList;
+      if (contactList && contactList.length > 0) {
+        contactList.forEach(function (contact) {
+          texts = "";
+          var contactClass = "";
+          if (contact.contact_class === "question") {
+            contactClass = "質問";
+          }
+          else if (contact.contact_class === "fault") {
+            contactClass = "障害";
+          }
+          else if (contact.contact_class === "request") {
+            contactClass = "要望";
+          }
+          else if (contact.contact_class === "nice") {
+            contactClass = "いいね！";
+          }
+          else if (contact.contact_class === "other") {
+            contactClass = "その他";
+          }
+          var contactStatus = "";
+          if (contact.status === "unsupported") {
+            contactStatus = "未対応";
+          }
+          else if (contact.status === "in_supported") {
+            contactStatus = "対応中";
+          }
+          else if (contact.status === "resolved") {
+            contactStatus = "解決済";
+          }
+          else if (contact.status === "pending") {
+            contactStatus = "保留";
+          }
+          texts += '<div class="col s12 m7">';
+          texts += '<div class="card horizontal">';
+          texts += '<div class="card-stacked">';
+          texts += '<div class="card-content">';
+          texts += '問合せ区分：' + contactClass;
+          texts += '<br>';
+          texts += '件名：<a href="../html/contact.html?' + contact.id + '&' + transitionId + '&' + surveyId + '&' + surveyDetailId + '">';
+          texts += convertSpace(contact.contact_name) + '</a>';
+          texts += '<br>';
+          texts += '問合せ状態：' + contactStatus;
+          texts += '<br>';
+          texts += '問合せ日時：' + contact.created_date;
+          texts += '</div>';
+          texts += '</div>';
+          texts += '</div>';
+          texts += '</div>';
+          contactItem.append(texts);
+        });
+      } else {
+        texts += '<div class="col s12 m7">';
+        texts += '<div class="card horizontal">';
+        texts += '<div class="card-stacked">';
+        texts += '<div class="card-content">';
+        texts += '<p>対象データが存在しません</p>';
+        texts += '</div>';
+        texts += '</div>';
+        texts += '</div>';
+        texts += '</div>';
+        contactItem.append(texts);
+      }
+      // 登録トースト表示
+      if (isRegister === 1) {
+        M.toast({ html: '登録しました！', displayLength: 2000 });
+      }
+    })
+    .fail(async (jqXHR, textStatus, errorThrown) => {
+      var jsonData     = JSON.stringify(jqXHR);
+      var responseData = JSON.parse(jsonData);
+      errorProcess(responseData);
+    })
+}
+
+/**
+ * 問合せ一覧取得結果がエラーだった場合の処理
+ * @param {object} responseData
+ */
+async function errorProcess(responseData) {
+  var message = '';
+  var error   = '';
+  if (responseData.status == 401) {
+    message = '申し訳ございません。セッションタイムアウトエラーが発生しました。再度ログインしてから処理を実行してください。';
+    error   = 'セッションタイムアウトエラー';
   }
-  contactItem.append(texts);
+  else if (responseData.status == 0) {
+    // API接続なし
+    // NW接続なし
+    message = 'ネットワークエラーが発生しました。ネットワークに接続されていることを確認し、もう一度「問合せする」ボタンをクリックしてください。確認後も同一の現象が発生する場合は、管理者へお問合せください。';
+    error   = 'サーバ接続エラー';
+  }
+  else {
+    message = '問合せ一覧取得処理が失敗しました。もう一度処理を実行してください。実行後も同一の現象が発生する場合は、管理者へお問合せください。';
+    error   = responseData.responseJSON.message;
+  }
+
+  $('#error').text(error);
+  $('#errorMessage').text(message);
+  $('#contactError').modal('open');
 }
 
 /**
  * 戻るリンク作成
- * @param {number} transitionId 遷移元番号
- * @param {string} surveyId 調査ID
- * @param {string} surveyDetailId 調査明細ID
  */
-function createBackLink(transitionId, surveyId, surveyDetailId) {
+function createBackLink() {
   var backLink     = $('#transition-view-link');
   var backLinkText = '';
   switch (transitionId) {
@@ -97,5 +196,12 @@ function createBackLink(transitionId, surveyId, surveyDetailId) {
       backLinkText = '<a href="../html/survey_data_edit.html?' + surveyId + '&' + surveyDetailId + '"><i class="material-icons">arrow_back_ios</i></a>';
   }
   backLink.append(backLinkText);
+}
+
+/**
+ * 問合せフォーム画面遷移
+ */
+function edit() {
+  location.href = "../html/contact.html?" + 0 + '&' + transitionId + '&' + surveyId + '&' + surveyDetailId;
 }
 
